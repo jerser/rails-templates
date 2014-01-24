d15b8f96c730effc191c3d0869d717808b10fd4c
Jeroen Serpieters <jeroen.serpieters@partheas.com>
2014-01-24 16:59:47 +0530

    Create Capfile in root instead of config/

--- spree.rb
+++ spree.rb
@@ -49,8 +49,8 @@ inside 'config' do
   gsub_file 'deploy.rb', /__PRODUCTION_USER__/, ask('Production user?')
   run 'wget https://raw.github.com/jerser/rails-templates/master/config/unicorn.rb'\
       ' -O unicorn.rb'
-  run 'echo -e "load \'deploy\'\nload \'config/deploy\'" > Capfile'
 end
+run 'echo -e "load \'deploy\'\nload \'config/deploy\'" > Capfile'
 
 git :init
 git add: '.'
